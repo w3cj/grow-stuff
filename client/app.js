54cd12d9d728843f2891927f8b772227bb44d64e
CJ R <cj@null.computer>
2017-06-02 15:33:14 -0600

    :sparkles: add loading image

--- client/app.js
+++ client/app.js
@@ -4,6 +4,8 @@ let gardeners = [];
 let currentGardener = 0;
 
 function appReady() {
+  $('.loading').show();
+  $('.card').hide();
   $.get('http://localhost:3000/gardeners')
     .then(result => {
       gardeners = result;
@@ -13,13 +15,22 @@ function appReady() {
   $('.next').click(() => {
     console.log('wat');
     currentGardener++;
+    if(currentGardener == gardeners.length) {
+      currentGardener = 0;
+    }
     showGardener();
   });
 }
 
 function showGardener() {
+  $('.loading').show();
+  $('.card').hide();
   const gardener = gardeners[currentGardener];
   $('#image').attr('src', gardener.img);
   $('#name').text(gardener.name);
   $('#bio').text(gardener.bio);
+  setTimeout(function() {
+    $('.loading').hide();
+    $('.card').show();
+  }, 2000);
 }

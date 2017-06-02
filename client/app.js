7dfd58f200a9069b5096773dbf2d1a60281e85c5
CJ R <cj@null.computer>
2017-06-02 14:58:38 -0600

    add front-end

--- client/app.js
+++ client/app.js
@@ -1,5 +1,25 @@
 $(appReady);
 
+let gardeners = [];
+let currentGardener = 0;
+
 function appReady() {
-  console.log('Hello from jquery!');
+  $.get('http://localhost:3000/gardeners')
+    .then(result => {
+      gardeners = result;
+      showGardener();
+    });
+
+  $('.next').click(() => {
+    console.log('wat');
+    currentGardener++;
+    showGardener();
+  });
+}
+
+function showGardener() {
+  const gardener = gardeners[currentGardener];
+  $('#image').attr('src', gardener.img);
+  $('#name').text(gardener.name);
+  $('#bio').text(gardener.bio);
 }
